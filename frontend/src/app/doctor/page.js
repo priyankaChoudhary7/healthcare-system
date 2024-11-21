2ab650ad73446e42bf3327b309336aac97e4cf26
shavez94 <shavezazmi.azmi@gmail.com>
2024-11-20 21:42:18 -0500

    2nd

---
+++ frontend/src/app/doctor/page.js
@@ -0,0 +1,38 @@
+"use client"; // Required to enable client-side behavior in App Router
+
+import { useRouter } from "next/navigation";
+
+const DoctorHomePage = () => {
+  const router = useRouter();
+
+  const navigateToAddPatient = () => {
+    router.push("doctor//add-patient"); // Navigate to the Add Patient page
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="bg-blue-600 text-white p-10 rounded-lg shadow-lg w-full max-w-lg">
+        <h1 className="text-center text-2xl font-bold mb-4">
+          DC Healthcare System
+        </h1>
+        <p className="text-center mb-6 text-xl">Welcome Doctor!</p>
+        <div className="space-y-4 text-center">
+          <button
+            onClick={navigateToAddPatient}
+            className="w-full bg-white text-blue-600 py-2 px-4 rounded-md shadow hover:bg-gray-100"
+          >
+            Add New Patient
+          </button>
+          <button className="w-full bg-white text-blue-600 py-2 px-4 rounded-md shadow hover:bg-gray-100">
+            View Patient History
+          </button>
+          <button className="w-full bg-white text-blue-600 py-2 px-4 rounded-md shadow hover:bg-gray-100">
+            Update Patient Information
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorHomePage;
