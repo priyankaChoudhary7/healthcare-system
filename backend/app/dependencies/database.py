a1d2ebba03b6eab49ba2d8dc49d4113897038e65
Priyanka Choudhary <52857047+priyankaChoudhary7@users.noreply.github.com>
2024-12-05 15:40:10 -0500

    env vars

--- backend/app/dependencies/database.py
+++ backend/app/dependencies/database.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 PG_DATABASE_URL = os.getenv("PG_DATABASE_URL")
-
+print(PG_DATABASE_URL)
 engine = create_engine(PG_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
